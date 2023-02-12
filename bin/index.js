3c76284bec4b2954b9ab0d23f58a36f2d7ec2cd0
KelvinQiu <kelvinqiu802@outlook.com>
2023-02-12 22:37:00 +0800

    feat: number arg

--- bin/index.js
+++ bin/index.js
@@ -4,16 +4,28 @@ const yargs = require('yargs');
 const { hideBin } = require('yargs/helpers');
 const { getTopN, storyCategorys } = require('./hn');
 
-const argv = yargs(hideBin(process.argv)).option('category', {
-  alias: 'c',
-  describe: 'News category',
-  type: 'string',
-  default: 'top',
-  choices: ['top', 'new', 'best'],
-}).argv;
+const argv = yargs(hideBin(process.argv))
+  .option('category', {
+    alias: 'c',
+    describe: 'News category',
+    type: 'string',
+    default: 'top',
+    choices: ['top', 'new', 'best'],
+  })
+  .option('number', {
+    alias: 'n',
+    describe: 'The number of news (1-500)',
+    type: 'number',
+    default: 10,
+  })
+  .check((args) => {
+    if (args.number < 0 || args.number > 500) {
+      throw new Error('The number must between 1~500');
+    } else return true;
+  }).argv;
 
 async function getNews() {
-  return await getTopN(5, storyCategorys[argv.category]);
+  return await getTopN(argv.number, storyCategorys[argv.category]);
 }
 
 getNews().then((data) => {
