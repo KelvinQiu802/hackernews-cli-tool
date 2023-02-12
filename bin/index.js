ac79e44f1136e31cbab0bc9680731e112e3fcda7
KelvinQiu <kelvinqiu802@outlook.com>
2023-02-12 22:23:48 +0800

    feat: add category argument

--- bin/index.js
+++ bin/index.js
@@ -2,5 +2,20 @@
 
 const yargs = require('yargs');
 const { hideBin } = require('yargs/helpers');
+const { getTopN, storyCategorys } = require('./hn');
 
-const argv = yargs(hideBin(process.argv)).argv;
+const argv = yargs(hideBin(process.argv)).option('category', {
+  alias: 'c',
+  describe: 'News category',
+  type: 'string',
+  default: 'top',
+  choices: ['top', 'new', 'best'],
+}).argv;
+
+async function getNews() {
+  return await getTopN(5, storyCategorys[argv.category]);
+}
+
+getNews().then((data) => {
+  data.forEach((data) => console.log(data.title));
+});
